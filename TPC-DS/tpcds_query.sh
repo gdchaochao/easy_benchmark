6f22a7defc589c92ab6d0be6113f0cdcc4afc024
duckzheng <duckzheng@tencent.com>
2019-03-08 13:31:47 +0800

    挑选出hive能用的的SQL，测试执行

---
+++ TPC-DS/tpcds_query.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+_WORKING_DIR="$(pwd)"
+echo $_WORKING_DIR
+
+# 输入参数
+while [ -n "$1" ]
+do
+  case "$1" in
+    --help)
+        echo "
+        --help     Show this help
+        --scale    The size of the generated data
+        --data     Directory for storing data
+        --result   Directory for storing result"
+        exit
+        ;;
+    --scale)
+        _DATA_SCALE=$2
+        shift
+        ;;
+    --data)
+        _DATA_DIR=$2
+        shift
+        ;;
+    --result)
+        _RESULT_DIR=$2
+        shift
+        ;;
+    *)
+        echo "$1 is not an option"
+        exit
+        ;;
+  esac
+  shift
+done
+
+if [ -z "$_DATA_SCALE" ]; then
+    _DATA_SCALE=10
+fi
+echo "The size of the generated data is：$_DATA_SCALE GB"
+
+if [ -z "$_DATA_DIR" ]; then
+    _DATA_DIR=$_WORKING_DIR/data
+fi
+mkdir -p $_DATA_DIR
+echo "Directory for storing data is：$_DATA_DIR"
+
+if [ -z "$_RESULT_DIR" ]; then
+    _RESULT_DIR=$_WORKING_DIR/result
+fi
+mkdir -p $_RESULT_DIR
+echo "Directory for storing result is：$_RESULT_DIR"
+
+
+echo "=========================================================="
+echo "start query..."
+echo "=========================================================="
+# start query
+timestamp=$(date +%s)
+echo "Timestamp:$timestamp"
+total_time_spent=0
+total_cpu_spent=0
+
+files=$(ls $_WORKING_DIR/resource/queries-test)
+echo "result in:$_RESULT_DIR/$timestamp"
+for filename in $files
+do
+#   echo $filename >> filename.txt
+   result_file=$_RESULT_DIR/$timestamp'_'${filename/.sql/}
+#   echo "result in:$result_file"
+   echo "Executing $filename now, please wait a moment"
+   hive -f $_WORKING_DIR/resource/queries/$filename > $result_file 2>&1
+   time_spent=$(cat $result_file | grep 'Time taken' | tr -cd "[0-9]\.")
+   cpu_spent=$(cat $result_file | grep 'MapReduce CPU Time Spent:')
+   cpu_spent=$(echo ${cpu_spent/seconds/\.} | tr -cd "[0-9]\.")
+   echo "cost time:$time_spent, cpu cost:$cpu_spent"
+   total_time_spent=$(awk 'BEGIN{printf "%.2f\n",('$total_time_spent'+'$time_spent')}')
+   total_cpu_spent=$(awk 'BEGIN{printf "%.2f\n",('$total_cpu_spent'+'$cpu_spent')}')
+   echo ${filename/.sql/}' '$time_spent' '$cpu_spent >> $_RESULT_DIR/$timestamp
+done
+echo "=========================================================="
+echo "Finish query..."
+echo "=========================================================="
+echo "total time:$total_time_spent"
+echo "total cpu time:$total_cpu_spent"
