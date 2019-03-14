2d08704ae485c8e566b7711a0781c95613f028f6
duckzheng <duckzheng@tencent.com>
2019-03-14 21:03:01 +0800

    加载数据也上报上云驭

--- TPC-DS/get_load_data_time.py
+++ TPC-DS/get_load_data_time.py
@@ -5,33 +5,30 @@ Created on 2019/3/13
 '''
 import re
 import sys
-import json
-from collections import defaultdict
 
 
 def get_load_data(r_file):
     total_time = 0
-    r = dict()
+    r = "\"{"
     table_pattern = r"Loading data to table .*\.(?P<value>[a-zA-Z\_]*)"
     time_pattern = r"Time taken: (?P<value>\d*(\.\d+)*)"
-    current_key = ""
     with open(r_file, 'r') as f:
         for line in f.readlines():
             if "Loading data to table" in line:
+                # print line.strip()
                 match = re.search(table_pattern, line.strip())
                 table = match.group("value")
-                current_key = table
+                r = r + "\\\"%s\\\":" % table
             if "Time taken" in line:
+                # print line.strip()
                 match = re.search(time_pattern, line.strip())
                 time = match.group("value")
                 total_time = total_time + float(time)
-                r[current_key] = time
-        r["#tpcds_load_time"] = total_time
-    # r = json.dumps(r)
+                r = r + "%s," % time
+        r = r + "\\\"#tpcds_load_time\\\":%d}\"" % total_time
     return r
 
 
 if __name__ == "__main__":
     # print get_load_data("./load_data_log")
     print get_load_data(sys.argv[1])
-
